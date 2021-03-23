008d0bfc8f94ad0298ff6c7dc44559491f7a8973
Imlas <sift2438@gmail.com>
2021-03-23 17:09:09 -0500

    Basic fading via editor commands works.

--- Assets/Scripts/Fadable.cs
+++ Assets/Scripts/Fadable.cs
@@ -9,7 +9,7 @@ public class Fadable : NetworkBehaviour
     /// This is a class dedicated to smoothly changing the opactiy of the material(s?) attatched to a gameobject, and syncing that between clients
     /// </summary>
 
-    [SerializeField] private Material mainMat; //This might be modified later as a List
+    [SerializeField] private MeshRenderer rend; //This might be modified later as a List
     [SerializeField] private float fadeTime;
     [SerializeField] private float unFadeTime;
 
@@ -17,44 +17,107 @@ public class Fadable : NetworkBehaviour
     private float startTime;
     private float curValue;
 
-    public void Fade()
+    public void FadeOut()
     {
-        Fade(fadeTime);
+        FadeOut(fadeTime);
     }
 
-    public void Fade(float _fadeTime)
+    public void FadeOut(float _fadeOutTime)
     {
         startTime = Time.time;
         curValue = 1f;
-        coroutine = SmoothFade(_fadeTime);
+        coroutine = SmoothFadeOut(_fadeOutTime);
         StartCoroutine(coroutine);
     }
 
-    private IEnumerator SmoothFade(float _fadeTime)
+    private IEnumerator SmoothFadeOut(float _fadeOutTime)
     {
+        //curValue will lerp from 1.0 to 0.0
         while(curValue >= 0f)
         {
-            curValue = ((Time.time - startTime) / _fadeTime);
-            Debug.Log($"{curValue}");
+            curValue = 1f - ((Time.time - startTime) / _fadeOutTime);
+
+            //We then get each material in the renderer, and set the alpha of the BaseColor to the curValue
+            SetAlphaTo(curValue);
+            yield return null;
         }
-        yield return null;
+
+        //In case we've overshot 0.0, we'll just set it to a flat 0. 
+        curValue = 0f;
+        SetAlphaTo(curValue);
+
     }
 
-    public void UnFade()
+    /// <summary>
+    /// Loops through all of the materials in this GameObjects linked MeshRenderer and sets the BaseColorAlpha to newAlpha
+    /// </summary>
+    /// <param name="newAlpha"></param>
+    public void SetAlphaTo(float newAlpha)
     {
-        UnFade(unFadeTime);
+        foreach (Material mat in rend.materials)
+        {
+            //Debug.Log($"Material color is {mat.GetColor("_BaseColor")}");
+            Color curColor = mat.GetColor("_BaseColor");
+            mat.SetColor("_BaseColor", new Color(curColor.r, curColor.g, curColor.b, newAlpha));
+        }
+
     }
 
-    public void UnFade(float _unFadeTime)
+    public void FadeIn()
+    {
+        FadeIn(unFadeTime);
+    }
+
+    public void FadeIn(float _fadeInTime)
     {
         startTime = Time.time;
+        curValue = 0.0f;
+        coroutine = SmoothFadeIn(_fadeInTime);
+        StartCoroutine(coroutine);
+
+    }
+
+    private IEnumerator SmoothFadeIn(float _fadeInTime)
+    {
+        //curValue will lerp from 0.0 to 1.0
+        while (curValue <= 1.0f)
+        {
+            curValue = ((Time.time - startTime) / _fadeInTime);
+
+            //We then get each material in the renderer, and set the alpha of the BaseColor to the curValue
+            SetAlphaTo(curValue);
+            yield return null;
+        }
+
+        //In case we've overshot 1.0, we'll just set it to a flat 1. 
+        curValue = 1f;
+        SetAlphaTo(curValue);
+
     }
 
 
-    [ContextMenu(("Fade Out over 3s"))]
-    private void EditorFadeTest()
+    [ContextMenu(("Fade Out"))]
+    private void EditorFadeOutTest()
     {
-        Fade(3f);
+        FadeOut();
+        //foreach(Material mat in rend.materials)
+        //{
+        //    Debug.Log($"Material color is {mat.GetColor("_BaseColor")}");
+        //    Color curColor = mat.GetColor("_BaseColor");
+        //    mat.SetColor("_BaseColor", new Color(curColor.r, curColor.g, curColor.b, 0.0f));
+        //}
+    }
+
+    [ContextMenu(("Fade In"))]
+    private void EditorFadeInTest()
+    {
+        FadeIn();
+        //foreach(Material mat in rend.materials)
+        //{
+        //    Debug.Log($"Material color is {mat.GetColor("_BaseColor")}");
+        //    Color curColor = mat.GetColor("_BaseColor");
+        //    mat.SetColor("_BaseColor", new Color(curColor.r, curColor.g, curColor.b, 0.0f));
+        //}
     }
 
 
